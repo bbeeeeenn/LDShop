d7d3ecb910432c431bf1af0043324f268e84d31f
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2025-04-12 13:30:52 +0800

    .

--- Commands/Shop.cs
+++ Commands/Shop.cs
@@ -34,10 +34,10 @@ public class Shop : Models.Command
                 ShopList(player);
                 break;
             case "buy":
-                // ShopBuy(player, args.Parameters);
+                ShopBuy(player, args.Parameters);
                 break;
             case "sell":
-                // ShopSell(player);
+                ShopSell(player);
                 break;
             default:
                 player.SendErrorMessage(
@@ -72,187 +72,185 @@ public class Shop : Models.Command
         );
     }
 
-    // private static void ShopBuy(TSPlayer player, List<string> parameters)
-    // {
-    //     if (parameters.Count < 2)
-    //     {
-    //         player.SendErrorMessage(
-    //             "[SHOP] Invalid syntax. Proper syntax: /shop buy <item index/name> [quantity]."
-    //         );
-    //         return;
-    //     }
+    private static void ShopBuy(TSPlayer player, List<string> parameters)
+    {
+        if (parameters.Count < 2)
+        {
+            player.SendErrorMessage(
+                "[SHOP] Invalid syntax. Proper syntax: /shop buy <item index/name> [quantity]."
+            );
+            return;
+        }
 
-    //     if (
-    //         !int.TryParse(parameters[1], out int itemIndex)
-    //         || itemIndex > ShopItems.GetUnlockedItemList().Count
-    //         || itemIndex < 1
-    //     )
-    //     {
-    //         player.SendErrorMessage("[SHOP] Invalid item index.");
-    //         return;
-    //     }
+        if (
+            !int.TryParse(parameters[1], out int itemIndex)
+            || itemIndex > ShopItems.GetUnlockedItemList().Count
+            || itemIndex < 1
+        )
+        {
+            player.SendErrorMessage("[SHOP] Invalid item index.");
+            return;
+        }
 
-    //     var itemPlace = ShopItems.GetItemPlace(itemIndex);
-    //     ShopItem itemFromShop = ShopItems.Shop.Items[itemPlace.key][itemPlace.index];
-    //     Item item = TShock.Utils.GetItemById(itemFromShop.netID);
-    //     item.prefix = itemFromShop.prefixID;
-    //     int quantity = 1;
+        ShopItems.ItemPlace itemPlace = ShopItems.GetItemPlace(itemIndex);
+        ShopItem itemFromShop = ShopItems.Shop.Items[itemPlace.key][itemPlace.index];
+        Item item = TShock.Utils.GetItemById(itemFromShop.netID);
+        item.prefix = itemFromShop.prefixID;
+        int quantity = 1;
 
-    //     if (parameters.Count >= 3 && int.TryParse(parameters[2], out int n) && n > 1)
-    //     // Get wanted quantity
-    //     {
-    //         if (n > 9999)
-    //         {
-    //             n = 9999;
-    //             player.SendInfoMessage(
-    //                 "[SHOP] You can only set the quantity to a maximum of 9999."
-    //             );
-    //         }
-    //         quantity = n;
-    //     }
+        if (parameters.Count >= 3 && int.TryParse(parameters[2], out int n) && n > 1)
+        // Get wanted quantity
+        {
+            if (n > 9999)
+            {
+                n = 9999;
+                player.SendInfoMessage(
+                    "[SHOP] You can only set the quantity to a maximum of 9999."
+                );
+            }
+            quantity = n;
+        }
 
-    //     if (itemFromShop.amount >= 0) // Skip if the item is infinite
-    //     {
-    //         if (itemFromShop.amount == 0)
-    //         {
-    //             player.SendErrorMessage("[SHOP] This item is out of stock.");
-    //             return;
-    //         }
-    //         else if (itemFromShop.amount < quantity)
-    //         {
-    //             player.SendInfoMessage(
-    //                 $"[SHOP] There's only x{itemFromShop.amount} of [i/p{itemFromShop.prefixID}:{itemFromShop.netID}] left in the shop."
-    //             );
-    //             quantity = itemFromShop.amount;
-    //         }
-    //     }
+        if (itemFromShop.amount >= 0) // Skips if the item is infinite
+        {
+            if (itemFromShop.amount == 0)
+            {
+                player.SendErrorMessage("[SHOP] This item is out of stock.");
+                return;
+            }
+            else if (itemFromShop.amount < quantity)
+            {
+                player.SendInfoMessage(
+                    $"[SHOP] There's only x{itemFromShop.amount} of [i/p{itemFromShop.prefixID}:{itemFromShop.netID}] left in the shop."
+                );
+                quantity = itemFromShop.amount;
+            }
+        }
 
-    //     long totalCost = itemFromShop.buyprice * quantity;
-    //     if (totalCost > LDEconomy.Variables.PlayerMoney[player.Account.Name])
-    //     {
-    //         player.SendErrorMessage(
-    //             $"[SHOP] You do not have enough balance to buy x{quantity} [i/p{itemFromShop.prefixID}:{itemFromShop.netID}]."
-    //         );
-    //         return;
-    //     }
+        long totalCost = itemFromShop.requirements.buyprice * quantity;
+        if (totalCost > LDEconomy.Variables.PlayerMoney[player.Account.Name])
+        {
+            player.SendErrorMessage(
+                $"[SHOP] You do not have enough balance to buy x{quantity} [i/p{itemFromShop.prefixID}:{itemFromShop.netID}]."
+            );
+            return;
+        }
 
-    //     // Check free inventory slots and give item
-    //     int freeSlots = 0;
-    //     bool uniqueItem = false;
+        // Check free inventory slots and give item
+        int freeSlots = 0;
+        bool uniqueItem = false;
 
-    //     if (item.maxStack == 1)
-    //         uniqueItem = true;
-    //     for (int i = 0; i < NetItem.InventorySlots - 1; i++)
-    //     // Loop through inventory slots
-    //     {
-    //         Item slot = player.TPlayer.inventory[i];
-    //         if (i > 49 && i < 54)
-    //             // Skip coin slots
-    //             continue;
-    //         if (!uniqueItem)
-    //         {
-    //             if (slot.netID == 0 || slot.netID == item.netID)
-    //             {
-    //                 if (!item.FitsAmmoSlot() && i > 53)
-    //                     continue;
-    //                 if (slot.netID == 0)
-    //                     freeSlots += item.maxStack;
-    //                 else
-    //                     freeSlots += slot.maxStack - slot.stack;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             if (slot.netID == 0 && i < 50)
-    //             {
-    //                 freeSlots++;
-    //             }
-    //         }
-    //     }
+        if (item.maxStack == 1)
+            uniqueItem = true;
+        for (int i = 0; i < NetItem.InventorySlots - 1; i++)
+        // Loop through inventory slots
+        {
+            Item slot = player.TPlayer.inventory[i];
+            if (i > 49 && i < 54)
+                continue; // Skip coin slots
 
-    //     if (freeSlots == 0)
-    //     {
-    //         player.SendErrorMessage("[SHOP] You have no available inventory space for this item.");
-    //         return;
-    //     }
-    //     if (freeSlots < quantity)
-    //     {
-    //         player.SendInfoMessage(
-    //             $"[SHOP] You only have {freeSlots} available inventory slots for this item."
-    //         );
-    //         quantity = freeSlots;
-    //         totalCost = itemFromShop.buyprice * quantity;
-    //     }
+            if (!uniqueItem)
+            {
+                if (slot.netID == 0 || slot.netID == item.netID)
+                {
+                    if (!item.FitsAmmoSlot() && i > 53)
+                        continue;
+                    if (slot.netID == 0)
+                        freeSlots += item.maxStack;
+                    else
+                        freeSlots += slot.maxStack - slot.stack;
+                }
+            }
+            else
+            {
+                if (slot.netID == 0 && i < 50)
+                {
+                    freeSlots++;
+                }
+            }
+        }
 
-    //     // Give the item
-    //     if (uniqueItem)
-    //         for (int i = 0; i < quantity; i++)
-    //             player.GiveItem(item.netID, 1, itemFromShop.prefixID);
-    //     else
-    //         player.GiveItem(item.netID, quantity);
+        if (freeSlots == 0)
+        {
+            player.SendErrorMessage("[SHOP] You have no available inventory space for this item.");
+            return;
+        }
+        if (freeSlots < quantity)
+        {
+            player.SendInfoMessage(
+                $"[SHOP] You only have {freeSlots} available inventory slots for this item."
+            );
+            quantity = freeSlots;
+            totalCost = itemFromShop.requirements.buyprice * quantity;
+        }
 
-    //     EconomyUtils.TakeMoney(player, totalCost);
-    //     player.SendSuccessMessage(
-    //         $"[SHOP] You successfully bought x{quantity} [i/p{itemFromShop.prefixID}:{itemFromShop.netID}] for {EconomyUtils.BalanceToCoin(totalCost)[1]}."
-    //     );
+        // Give the item
+        if (uniqueItem)
+            for (int i = 0; i < quantity; i++)
+                player.GiveItem(item.netID, 1, itemFromShop.prefixID);
+        else
+            player.GiveItem(item.netID, quantity);
 
-    //     ShopItems.ItemPlace itemPlaceInShop = ShopItems.GetItemPlace(itemIndex);
-    //     ShopItem shopItem = ShopItems.Shop.Items[itemPlaceInShop.key][itemPlaceInShop.index];
-    //     shopItem.amount -= quantity;
-    //     ShopItems.Shop.Items[itemPlaceInShop.key][itemPlaceInShop.index] = shopItem;
-    //     ShopItems.SaveShop();
-    // }
+        EconomyUtils.TakeMoney(player, totalCost);
+        player.SendSuccessMessage(
+            $"[SHOP] You successfully bought x{quantity} [i/p{itemFromShop.prefixID}:{itemFromShop.netID}] for {EconomyUtils.BalanceToCoin(totalCost)[1]}."
+        );
 
-    // private static void ShopSell(TSPlayer player)
-    // {
-    //     Item selectedItem = player.SelectedItem;
-    //     ShopItem shopItem = ShopItems
-    //         .GetUnlockedItemList()
-    //         .FirstOrDefault(item => item.netID == selectedItem.netID);
+        itemFromShop.amount -= quantity;
+        ShopItems.Shop.Items[itemPlace.key][itemPlace.index] = itemFromShop;
+        ShopItems.SaveShop();
+    }
 
-    //     if (selectedItem.netID == 0)
-    //     {
-    //         player.SendErrorMessage(
-    //             "[SHOP] Please select an item in your hotbar and then type /shop sell."
-    //         );
-    //         return;
-    //     }
+    private static void ShopSell(TSPlayer player)
+    {
+        Item selectedItem = player.SelectedItem;
+        ShopItem shopItem = ShopItems
+            .GetUnlockedItemList()
+            .FirstOrDefault(item => item.netID == selectedItem.netID);
 
-    //     if (shopItem.sellprice == 0)
-    //     {
-    //         player.SendErrorMessage("[SHOP] Sorry, we are not buying that item.");
-    //         return;
-    //     }
+        if (selectedItem.netID == 0)
+        {
+            player.SendErrorMessage(
+                "[SHOP] Please select an item in your hotbar and then type /shop sell."
+            );
+            return;
+        }
 
-    //     if (shopItem.amount >= 0)
-    //     {
-    //         shopItem.amount += selectedItem.stack;
-    //         foreach (var (key, list) in ShopItems.Shop.Items)
-    //         {
-    //             bool done = false;
-    //             for (int i = 0; i < list.Count; i++)
-    //             {
-    //                 if (list[i].netID == shopItem.netID)
-    //                 {
-    //                     ShopItems.Shop.Items[key][i] = shopItem;
-    //                     done = true;
-    //                     break;
-    //                 }
-    //             }
-    //             if (done)
-    //                 break;
-    //         }
-    //     }
-    //     ShopItems.SaveShop();
+        if (shopItem.sellreturn.sellprice == 0)
+        {
+            player.SendErrorMessage("[SHOP] Sorry, we are not buying that item.");
+            return;
+        }
 
-    //     long totalCost = shopItem.sellprice * selectedItem.stack;
-    //     EconomyUtils.GiveMoney(player, totalCost);
-    //     player.SendSuccessMessage(
-    //         $"[SHOP] You successfully sold x{selectedItem.stack} [i/p{selectedItem.prefix}:{shopItem.netID}] for {EconomyUtils.BalanceToCoin(totalCost)[1]}."
-    //     );
+        if (shopItem.amount >= 0)
+        {
+            shopItem.amount += selectedItem.stack;
+            foreach (var (key, list) in ShopItems.Shop.Items)
+            {
+                bool done = false;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].netID == shopItem.netID)
+                    {
+                        ShopItems.Shop.Items[key][i] = shopItem;
+                        done = true;
+                        break;
+                    }
+                }
+                if (done)
+                    break;
+            }
+        }
+        ShopItems.SaveShop();
 
-    //     selectedItem.stack = 0;
-    //     for (byte i = 0; i < NetItem.InventorySlots; i++)
-    //         player.SendData(PacketTypes.PlayerSlot, "", player.Index, i);
-    // }
+        long totalCost = shopItem.sellreturn.sellprice * selectedItem.stack;
+        EconomyUtils.GiveMoney(player, totalCost);
+        player.SendSuccessMessage(
+            $"[SHOP] You successfully sold x{selectedItem.stack} [i/p{selectedItem.prefix}:{shopItem.netID}] for {EconomyUtils.BalanceToCoin(totalCost)[1]}."
+        );
+
+        selectedItem.stack = 0;
+        for (byte i = 0; i < NetItem.InventorySlots; i++)
+            player.SendData(PacketTypes.PlayerSlot, "", player.Index, i);
+    }
 }
